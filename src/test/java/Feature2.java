eb5ff9eb9392f46d6ae7967dd119ef1f1904e3ef
tiger1911 <mehmetatas@yahoo.com>
2020-10-13 18:47:05 +0200

    Awesome feature 2 more added

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -3,5 +3,7 @@ public class Feature2 {
     public static void main(String[] args) {
 
         System.out.println("Working on Feature2");
+        System.out.println("Working on feature2 more");
+        System.out.println("Working on feature2 again");
     }
 }
