142dd68981f35dedf4433b80b24b95fb3a27da30
tiger1911 <65711963+tiger1911@users.noreply.github.com>
2020-10-13 19:26:40 +0200

    Added changes to Hello from github

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Still really awesome hello");
+        System.out.println("Still really FROM GITHUB MADE SOME CHANGES awesome hello");
+        
 
     }
 }
