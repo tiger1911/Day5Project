402529b87881a39c28f193700497b8431d877cec
tiger1911 <mehmetatas@yahoo.com>
2020-10-13 18:56:19 +0200

    Awesome feature 2 added more changes line 3

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -5,5 +5,7 @@ public class Feature2 {
         System.out.println("Working on Feature2");
         System.out.println("Working on feature2 more");
         System.out.println("Working on feature2 again");
+
+        System.out.println("Adding some more changes to line 3, vitually");
     }
 }
