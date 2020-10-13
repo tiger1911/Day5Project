a73fa6399d5afcead091c7d40a7086c02be6e8ad
tiger1911 <mehmetatas@yahoo.com>
2020-10-13 19:41:50 +0200

    added comments

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Still really awesome hello"); // add one more line and commit
+        System.out.println("Still really awesome hello"); // add one more line and committing
 
 
     }
